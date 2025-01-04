3237f646e479f4872438c099630b4a2176967d16
moiz2405 <km856307@gmail.com>
2025-01-04 13:56:47 +0000

    Update app page with necessary changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import Hero from '../components/home/Hero'
-
+import { SiteProvider } from '../context/SiteContext';
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
-      <Hero />
+      <SiteProvider>
+        <Hero /> {/* Wrap Hero with SiteProvider */}
+      </SiteProvider>
     </main>
   )
 }
