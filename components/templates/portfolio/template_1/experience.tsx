02200a887801348fb150c69acc2303f16b8e2cb8
moiz2405 <km856307@gmail.com>
2025-01-04 15:42:50 +0000

    Remove outdated template files from Template 1 and Template 2

--- components/templates/portfolio/template_1/experience.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function experience() {
-  return (
-    <div>experience</div>
-  )
-}
-
-export default experience
--- components/templates/portfolio/template_1/herosection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function herosection() {
-  return (
-    <div>herosection</div>
-  )
-}
-
-export default herosection
