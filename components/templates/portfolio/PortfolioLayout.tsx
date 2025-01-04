def7005682f33944ec6f7982601a500f882c4963
moiz2405 <km856307@gmail.com>
2025-01-04 16:45:01 +0000

    Modify PortfolioLayout component

--- components/templates/portfolio/PortfolioLayout.tsx
+++ components/templates/portfolio/PortfolioLayout.tsx
@@ -6,35 +6,20 @@ import SkillsSection from '../portfolio/SkillsSection';
 import ProjectsSection from '../portfolio/ProjectsSection';
 import ExperienceSection from '../portfolio/ExperienceSection';
 
-
-
 type StyleType = 'style1' | 'style2' | 'style3';
 
 export default function PortfolioLayout() {
-    const [heroStyle, setHeroStyle] = useState<StyleType>('style1');
-    const [skillsStyle, setSkillsStyle] = useState<StyleType>('style1');
-    const [projectsStyle, setProjectsStyle] = useState<StyleType>('style1');
-    const [experienceStyle, setExperienceStyle] = useState<StyleType>('style1');
+    const [heroStyle] = useState<StyleType>('style1');
+    const [skillsStyle] = useState<StyleType>('style1');
+    const [projectsStyle] = useState<StyleType>('style1');
+    const [experienceStyle] = useState<StyleType>('style1');
 
     return (
         <div className="w-full max-w-6xl mx-auto space-y-16 py-12">
-            <HeroSection
-                style={heroStyle}
-                onStyleChange={(style) => setHeroStyle(style as StyleType)}
-            />
-            <SkillsSection
-                style={skillsStyle}
-                onStyleChange={(style) => setSkillsStyle(style as StyleType)}
-            />
-            <ProjectsSection
-                style={projectsStyle}
-                onStyleChange={(style) => setProjectsStyle(style as StyleType)}
-            />
-            <ExperienceSection
-                style={experienceStyle}
-                onStyleChange={(style) => setExperienceStyle(style as StyleType)}
-            />
+            <HeroSection style={heroStyle} />
+            <SkillsSection style={skillsStyle} />
+            <ProjectsSection style={projectsStyle} />
+            <ExperienceSection style={experienceStyle} />
         </div>
     );
 }
-
