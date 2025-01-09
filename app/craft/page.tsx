bbad4e276cfe30d129712dfad3986b3be2179411
moiz2405 <km856307@gmail.com>
2025-01-09 19:14:50 +0000

    updated templates

--- app/craft/page.tsx
+++ app/craft/page.tsx
@@ -2,9 +2,10 @@
 
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation'; // Import useSearchParams
+import { Suspense } from 'react'; // Import Suspense
 import CraftSite from '../../components/craft/CraftSite';
 
-export default function Page() {
+const PageContent = () => {
     const searchParams = useSearchParams(); // Access query parameters
     const [selectedSite, setSelectedSite] = useState<string>('Portfolio');
 
@@ -18,5 +19,13 @@ export default function Page() {
         <div>
             <CraftSite selectedSite={selectedSite} />
         </div>
+    );
+};
+
+export default function Page() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <PageContent />
+        </Suspense>
     );
 }
