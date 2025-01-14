3ddbb6382bd99ac6428875e8cb77718bde43d767
moiz2405 <km856307@gmail.com>
2025-01-14 14:32:39 +0000

    updated template

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -4,13 +4,7 @@ import { HeroSection } from '@/components/templates/portfolio/HeroSection';
 export default function Page() {
     return (
         <>
-            <HeroSection
-                title="Welcome to My Portfolio"xcfsfasdf
-                subtitle="Explore My Work and Achievements"
-                buttonText1="Contact Me"
-                buttonText2="View Projects"
-                buttonLink1="#contact"
-                buttonLink2="#projects"
+            <HeroSectionsad
                 socialLinks={{
                    asd asd yourusername",asd asd 
                     linkedin: "https://www.linkedin.com/in/yourusername"
