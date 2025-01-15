ec0bbef332113bb99d9ef2f891edcc031dc03c48
moiz2405 <km856307@gmail.com>
2025-01-15 18:05:38 +0000

    Update ESLint configuration

--- app/customize/page.tsx
+++ app/customize/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Suspense } from 'react';
-import CustomizePageContent from '../../components/customize/';
+// import CustomizePageContent from '../../components/customize/CustomizePageContent';
 
 export default function CustomizePage() {
     return (
         <Suspense fallback={<div>Loading customization options...</div>}>
-            <CustomizePageContent />
+            {/* <CustomizePageContent /> */}
         </Suspense>
     );
 }
